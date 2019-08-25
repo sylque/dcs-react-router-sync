20876e4dee919bb593feaf3c5c6004235bf5d100
sylque <sylvain.quendez@gmail.com>
2019-08-25 19:06:47 +0200

    New feature: user-defined scrollIntoView function

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ let g_routeMatcher = null
 let g_discourseDidThis = false
 let g_discoursePushedData = null
 const g_components = new Set()
-let g_prevSelectedNode = null
-let g_selectedNode = null
 
 //------------------------------------------------------------------------------
 
-// This function is designed to run as much code as possible when there
-// is no iframe. THIS IS FOR DEBUGGING PURPOSE, so that the package can be
-// tested without an iframe. I know the code could have been simpler with
-// a single "if (!inIFrame()) return" at the begining.
 exports.runReactRouterSync = ({ browserHistory, routeMatcher }) => {
   if (g_routeMatcher) {
     throwError('"runReactRouterSync" should be called only once')
   }
   g_routeMatcher = routeMatcher
 
+  // This function is designed to run as much code as possible when there
+  // is no iframe. THIS IS FOR DEBUGGING PURPOSE, so that the package can be
+  // tested without an iframe. I know the code could have been simpler with
+  // a single "if (!inIFrame()) return" at the beginning.
+
   //***** Handle internal React route changes *****
-  // Parse the url, extract Docuss query params and set the Discourse route accordingly.
+  // Parse the url, extract Docuss query params and set the Discourse route
+  // accordingly.
   // This is *not* called at load time, which is good, because Discourse is the
   // leader and we want to react to its first onDiscourseRoutePushed instead.
   browserHistory.listen(async (location, method) => {
@@ -37,17 +37,14 @@ exports.runReactRouterSync = ({ browserHistory, routeMatcher }) => {
 
     // Check query params
     if (!!interactMode !== (layout === 2 || layout === 3)) {
-      throwError('invalid query param (dcs-layout or dcs-interact-mode)')
+      throwError('invalid query param dcs-layout or dcs-interact-mode')
     }
     if (triggerId && !(layout === 2 || layout === 3)) {
-      throwError('invalid query param (dcs-layout or dcs-trigger-id)')
+      throwError('invalid query param dcs-layout or dcs-trigger-id')
     }
-    if (layout && (layout < 0 || layout === 1 || layout > 3)) {
-      throwError('unsupported query param (dcs-layout)')
+    if (layout < 0 || layout > 3) {
+      throwError('invalid query param dcs-layout')
     }
-
-    // Update the "selected" state in registered components
-    g_components.forEach(c => c._updateSelected(triggerId))
 
     if (g_discourseDidThis) {
       return
@@ -73,160 +70,141 @@ exports.runReactRouterSync = ({ browserHistory, routeMatcher }) => {
   }
 
   //***** Handle Discourse route changes *****
-  // When a route change is triggerd by Disourse, we need to change the React
+  // When a route change is triggered by Discourse, we need to change the React
   // Router route
   comToPlugin.onDiscourseRoutePushed(
     async ({ route, descr, counts, clientContext }) => {
-      // Store the pushed data and update registered components (see withDcs below)
-      g_discoursePushedData = { currentRoute: route, counts }
-      g_components.forEach(c => c._updateCount())
+      g_discoursePushedData = { route, counts }
 
-      if (clientContext && clientContext.iDidThis) {
-        return
-      }
+      if (!clientContext || !clientContext.iDidThis) {
+        // Get the pathname corresponding to the new route
+        const pathname =
+          route.layout === 1
+            ? route.pathname
+            : await routeMatcher.getPathname(route.pageName)
+        if (!pathname) {
+          const error = `Cannot find pathname for page "${route.pageName}"`
+          comToPlugin.postSetRouteProps({ error })
+          throwError(error)
+        }
 
-      if (route.layout === 1) {
-        return
-      }
-
-      // Get the pathname corresponding to the new route
-      const pathname = await routeMatcher.getPathname(route.pageName)
-      if (!pathname) {
-        const error = `Cannot find pathname for page "${route.pageName}"`
-        comToPlugin.postSetRouteProps({ error })
-        throwError(error)
-      }
-
-      // Add the correct query params. It is not necessary for this module,
-      // but required for users to know the current route.
-      let params = ''
-      if (route.layout !== 0) {
-        params = `?dcs-layout=${route.layout}&dcs-interact-mode=${
-          route.interactMode
-        }`
+        // Add the correct query params. It is not necessary, but nice to have
+        // for debug purpose
+        let params = `?dcs-layout=${route.layout}`
+        if (route.interactMode) {
+          params += `&dcs-interact-mode=${route.interactMode}`
+        }
         if (route.triggerId) {
           params += `&dcs-trigger-id=${route.triggerId}`
         }
-      }
-      // This test is necessary. Indeed, browserHistory.replace() actually
-      // performs a replace even if the url hasn't change, which in turn
-      // call the history.listen() hook.
-      if (location.pathname !== pathname || location.search !== params) {
-        // Don't store this in the history state, otherwise it will interfere
-        // when user clicks the back button
-        g_discourseDidThis = true
 
-        browserHistory.replace(pathname + params)
-
-        g_discourseDidThis = false
+        // This test is necessary. Indeed, browserHistory.replace() actually
+        // performs a replace even if the url hasn't change, which in turn
+        // call the history.listen() hook.
+        if (location.pathname !== pathname || location.search !== params) {
+          // Don't store g_discourseDidThis in the history state, otherwise it
+          // will interfere when user clicks the back button
+          g_discourseDidThis = true
+          browserHistory.replace(pathname + params)
+          g_discourseDidThis = false
+        }
       }
+
+      // Update all WithDcs components with the new route
+      g_components.forEach(c => c._updateState())
     }
   )
-
-  // Resize event with debounce
-  // https://developer.mozilla.org/en-US/docs/Web/Events/resize#setTimeout
-  window.addEventListener('resize', evt => {
-    if (this.resizeTimer !== null) {
-      clearTimeout(this.resizeTimer)
-    }
-    this.resizeTimer = setTimeout(() => {
-      this.resizeTimer = null
-      const node = g_selectedNode || g_prevSelectedNode
-      if (node) {
-        node.scrollIntoView({ behavior: 'smooth' })
-      }
-    }, 100)
-  })
 }
 
 //------------------------------------------------------------------------------
 
-// Return a modified WrappedComponent with additional dcsCount and dcsSelected properties
-// - pathname is optional. If not provided, the pageName corresponding to the current page is used.
-// - The triggerId, if there is one, must be passed as a prop
+// Return a modified WrappedComponent with additional dcsCount and dcsSelected
+// properties
+// - pathname is optional. If not provided, the pageName corresponding to the
+// current page is used.
+// - The triggerId, if any, must be passed as a prop
 exports.withDcs = (WrappedComponent, pathname = undefined) =>
   class WithDcs extends React.Component {
     constructor(props) {
       super(props)
 
+      // Check init
       if (!g_routeMatcher) {
         throwError('please call "runReactRouterSync" before using "withDcs"')
       }
 
+      // Get the pageName
       this.pageNameFromPathname =
         pathname && g_routeMatcher.getPageName(pathname)
 
-      const dcsSelected =
-        new URLSearchParams(location.search).get('dcs-trigger-id') ===
-        this.props.triggerId
+      // Init the component state
+      this.state = { dcsCount: undefined, dcsSelected: false }
+    }
 
-      this.state = { dcsCount: undefined, dcsSelected }
-
+    componentDidMount() {
+      // Store the component in a global list
       g_components.add(this)
 
+      // Update the state if a route has already been received
       if (g_discoursePushedData) {
-        this._updateCount(true)
+        this._updateState()
       }
     }
 
     componentWillUnmount() {
+      // Remove the component from the global list
       g_components.delete(this)
     }
 
-    componentDidMount() {
-      if (this.state.dcsSelected) {
-        // Scroll the component into view
-        g_selectedNode = ReactDOM.findDOMNode(this)
-        g_selectedNode.scrollIntoView()
-      }
-    }
-
-    async _updateCount(init = false) {
-      if (this.state.dcsCount !== undefined) {
-        return
-      }
-
-      const { counts, currentRoute } = g_discoursePushedData
-
-      const triggerId = this.props.triggerId
-
-      const pageName = this.pageNameFromPathname
-        ? await this.pageNameFromPathname
-        : currentRoute.pageName
-
-      if (!pageName) {
-        return
-      }
-
-      const found = counts.find(
-        c =>
-          c.pageName === pageName &&
-          (triggerId === undefined || c.triggerId === triggerId)
+    shouldComponentUpdate(nextProps, nextState) {
+      return (
+        nextState.dcsSelected !== this.state.dcsSelected ||
+        nextState.dcsCount !== this.state.dcsCount
       )
-      const dcsCount = found ? found.count : 0
-
-      // If we awaited for this.pageNameFromPathname, we are not in the
-      // constructor anymore!
-      if (init && !this.pageNameFromPathname) {
-        this.state.dcsCount = dcsCount
-      } else {
-        this.setState({ dcsCount })
-      }
     }
 
-    _updateSelected(selectedTriggerId) {
+    async _updateState() {
+      const { route, counts } = g_discoursePushedData
+
+      // See if the component is selected
       const dcsSelected =
-        this.props.triggerId && this.props.triggerId === selectedTriggerId
-      if (this.state.dcsSelected !== dcsSelected) {
-        this.setState({ dcsSelected })
-        if (dcsSelected) {
-          g_prevSelectedNode = g_selectedNode
-          g_selectedNode = ReactDOM.findDOMNode(this)
-        } else if (!selectedTriggerId) {
-          g_prevSelectedNode = g_selectedNode
-          g_selectedNode = null
+        !!route.triggerId && route.triggerId === this.props.triggerId
+
+      // Get the count (only the first time)
+      let dcsCount = this.state.dcsCount
+      if (dcsCount === undefined) {
+        const pageName = this.pageNameFromPathname
+          ? await this.pageNameFromPathname
+          : route.pageName
+        if (pageName) {
+          const found = counts.find(
+            c =>
+              c.pageName === pageName &&
+              (this.props.triggerId === undefined ||
+                c.triggerId === this.props.triggerId)
+          )
+          dcsCount = found ? found.count : 0
         }
       }
+
+      // Update the component state
+      const previouslySelected = this.state.dcsSelected
+      this.setState({ dcsCount, dcsSelected }, () => {
+        // Scroll to the selected trigger, even if it was already selected
+        // (because layout change requires scrolling again).
+        // Also, if no trigger is selected anymore, scroll to the previously
+        // selected one.
+        if (dcsSelected || (previouslySelected && !route.triggerId)) {
+          // Wait for the split bar animation to end, then scroll to the selected node
+          setTimeout(() => {
+            const node = ReactDOM.findDOMNode(this)
+            // The dcsScrollIntoView property allows to override the default 
+            // scroll behavior
+            const scrollFn = this.props.dcsScrollIntoView || defaultScrollIntoView
+            scrollFn(node, route)
+          }, 500)
+        }
+      })
     }
 
     render() {
@@ -237,6 +215,16 @@ exports.withDcs = (WrappedComponent, pathname = undefined) =>
       })
     }
   }
+
+//------------------------------------------------------------------------------
+
+function defaultScrollIntoView(node, route) {
+  const rect = node.getBoundingClientRect()
+  const partiallyVisible = rect.top < window.innerHeight && rect.bottom >= 0
+  if (!partiallyVisible) {
+    node.scrollIntoView({ behavior: 'smooth' })
+  }
+}
 
 //------------------------------------------------------------------------------
 
